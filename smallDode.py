8510cb6c65962a4afa21ace049880d907aff4e89
unknown <davidwbulger@gmail.com>
2021-01-03 21:54:16 +1100

    Have refactored the teeth. Yet to tidy or cut & calibrate.

--- smallDode.py
+++ smallDode.py
@@ -8,7 +8,9 @@ import matplotlib.pyplot as plt
 #   Blank.gcode: cuts a blank, using a wider drill bit
 
 
+##################################################################################################################
 #### ##    PARAMETERS:    ## #####
+##################################################################################################################
 
 blankCutDepth = 6 # 14  #  board seems to be 10mm, and bit radius is 3mm; this is 1mm extra
 blankNumPasses = 3 # 3
@@ -17,14 +19,16 @@ excess = 0 # 2  #  excess, in mm, around the margin of the pentagon blanks cut i
 rf = 57 # 67  #  planar exradius of face at outside [see Appendix A]
 th = 9  #  thickness of board
 originHeight = 20
+ballrad = 1.0
+gcodeToolSeq = [{'bitrad':ballrad,'cude':1.5,'ds':1}]
 
 boolPlot = False
 
 ## TOOTH WIDTH/SHAPE STUFF:
 
-# After a failed first try (I believe due to numerical precision errors) I'm now fixing the tooth width as the width of the drill
-# bit---essentially. The true bit width will be the mortisse width, and the tooth width will be slightly smaller, with the difference
-# determined by glueGapHack.
+# After a failed first try (I believe due to numerical precision errors) I'm now fixing the tooth width as the
+# width of the drill bit---essentially. The true bit width will be the mortisse width, and the tooth width will be
+# slightly smaller, with the difference determined by glueGapHack.
 glueGapHack = 0.001 # 0.03
 realBallRad = 1.17  #  it's REALLY 1.0, but this seems to be the half-width of the cut, between teeth at least
 
@@ -32,7 +36,6 @@ realBallRad = 1.17  #  it's REALLY 1.0, but this seems to be the half-width of t
 # glueGapHack = 0.06 # 0.03
 # mortiseWidthHack = 1.19 # 0.17
 rf = 24
-# if cutsPerTooth%4 != 2: raise ValueError("cutsPerTooth must be twice an odd number.")
 
 #### ##    CALCULATED VALUES:    ## #####
 
@@ -40,12 +43,14 @@ ird = rf*np.sqrt((7+3*np.sqrt(5))/8)  # solid inradius to exterior face
 irf = rf*np.cos(np.pi/5)  #  planar inradius to exterior edge
 phi = (1+np.sqrt(5))/2
 
+##################################################################################################################
 #### ##    CREATE THE GCODE TO CUT OUT THE BLANKS:    ## #####
+##################################################################################################################
 
 # This would be used 12 times to cut pentagonal blanks that are a bit too big.
 
-verts = 0.5*(rf+blankbitrad+excess)*np.array([[phi, np.sqrt(3-phi)], [1-phi,np.sqrt(phi+2)], [-2,0], [1-phi,-np.sqrt(phi+2)],
-  [phi,-np.sqrt(3-phi)]])
+verts = 0.5*(rf+blankbitrad+excess)*np.array([[phi, np.sqrt(3-phi)], [1-phi,np.sqrt(phi+2)], [-2,0],
+  [1-phi,-np.sqrt(phi+2)], [phi,-np.sqrt(3-phi)]])
 bnodes = np.zeros((3,7+6*blankNumPasses))
 bnodes[:,:4] = np.array([[0,0,0],[0,0,5],[*verts[0,:],5],[*verts[0,:],0]]).T
 bnodes[:,-3:] = np.array([[*verts[0,:],5], [0,0,5], [0,0,0]]).T 
@@ -62,16 +67,16 @@ btp.PathToGCode(500, "Blank.gcode")
 
 #### ##    CREATE THE GCODE TO MARK THE SPOILBOARD WITH A PENTAGON FOR POSITIONING:    ## #####
 
-# After cutting the blanks, secure a big piece of spoiler board to the base board and then carve this shallow "guide" into it to help
-# position the first blank. The first joint cut will obliterate the guide, but it will leave an equally effective visual indication of
-# where to put the remaining blanks.
+# After cutting the blanks, secure a big piece of spoiler board to the base board and then carve this shallow
+# "guide" into it to help position the first blank. The first joint cut will obliterate the guide, but it will
+# leave an equally effective visual indication of where to put the remaining blanks.
 
 # Remember:
 #   don't change the XY position from cutting the guide to the end of cutting ALL blanks' edges;
-#   manually select grain orientation for 2JE, 3JE and 4JE blanks (joint edges are anticlockwise starting at right edge).
+#   manually select grain orientation for 2JE, 3JE & 4JE blanks (joint edges are anticlockwise from right edge).
 
-verts = 0.5*(rf+1.3+excess)*np.array([[phi, np.sqrt(3-phi)], [1-phi,np.sqrt(phi+2)], [-2,0], [1-phi,-np.sqrt(phi+2)],
-  [phi,-np.sqrt(3-phi)]])
+verts = 0.5*(rf+1.3+excess)*np.array([[phi, np.sqrt(3-phi)], [1-phi,np.sqrt(phi+2)], [-2,0],
+  [1-phi,-np.sqrt(phi+2)], [phi,-np.sqrt(3-phi)]])
 bnodes = np.zeros((3,7+6*1))
 bnodes[:,:4] = np.array([[0,0,0],[0,0,5],[*verts[-1,:],5],[*verts[-1,:],0]]).T
 bnodes[:,-3:] = np.array([[*verts[-1,:],5], [0,0,5], [0,0,0]]).T 
@@ -84,7 +89,9 @@ btaxis = np.array([[0, 3, 10], [0,1,0]])
 btp = cnc.ToolPath(bnodes, btaxis)
 btp.PathToGCode(300, "Guide.gcode")
 
+##################################################################################################################
 #### ##    CREATE THE TOOLPATH FOR THE EDGES WITH TEETH:    ## #####
+##################################################################################################################
 
 # ## Tooth width/shape stuff:
 # # We'll tell the cnc module that this is the bit radius, to persuade it to leave room for glue:
@@ -135,43 +142,62 @@ btp.PathToGCode(300, "Guide.gcode")
 # Parameters for tenon & mortise shape:
 edgePropJoined = 0.5 # 0.9  #  max proportion of edge used for finger joint
 mitreMargin = 1  #  vertically, for simplicity
-ballrad = 1.0
-cutsPerTooth = 5
-mortiseWidth = 2.35  #  Ideally this would be twice the bit radius, but in practice the bit carves out a little more.
+cutsPerTooth = 6
+if cutsPerTooth%4 != 2: raise ValueError("cutsPerTooth must be twice an odd number.")
+mortiseWidth = 2.35  #  This should be 2ce the bit radius, but in practice the bit carves out a little more.
 toothWidth = 2.15
-brachidont = 0.3  #  Make the tooth "shorter" (in the direction normal to the "joint plane" described by xzflat below) by this amount.
+offset = (mortiseWidth + toothWidth) / cutsPerTooth
+# Make the tooth "shorter" (in the direction normal to the "joint plane" described by xzflat) by this amount:
+brachidont = 0.3
+
+if (1-2/cutsPerTooth) * (mortiseWidth+toothWidth) / 2 >= 2*ballrad:
+  raise ValueError("These values might give multiple cuts within the mortise.")
 
 # Calculated values:
-v = 0.5*(th-mitreMargin)*(1-(irf/ird)**2)  #  length of vertical (i.e., face-normal) edge of mortise
-ctc = np.array([irf*(1-mitreMargin/ird), -th+mitreMargin+v])
-
+edgeHalfLen = rf*np.sin(np.pi/5)  #  exact at exterior (bottom) face
+numTeeth = int(np.floor(2*edgeHalfLen*edgePropJoined/(mortiseWidth+toothWidth)))
+v = 0.5*(th-mitreMargin)*(1+(irf/ird)**2)  #  length of vertical (i.e., face-normal) edge of mortise
+# Determine the centre of the arc at the tooth's apex:
+brachshift = (brachidont/np.linalg.norm([ird,irf])) * np.array([ird,irf])
+ctc = (np.array([irf*(1-mitreMargin/ird), -th+mitreMargin+v])  #  mirror image of nadir of mortise
+  - ballrad * np.array([1, irf/ird])  #  centre of circle tangent to edges of mortise's mirror image
+  - brachshift)  #  shorten tooth for fit tolerance
+toothBaseShrink = brachshift[0] * np.array([-1,ird/irf])  #  shift of tenon's base vertex relative to mortise's
+rho = np.linspace(2*np.arctan(irf/ird), 0, 8)
 
 xzflat = np.vstack((irf*(1-np.array([th+2*ballrad,-2*ballrad])/ird), np.array([2*ballrad,-th-2*ballrad])))
 xzdown = np.vstack((irf*(1-np.array([th+2*ballrad,th,th,mitreMargin,-2*ballrad])/ird),
   np.array([2*ballrad,0,-v,-th+mitreMargin,-th-2*ballrad])))
+xzup = np.column_stack((xzdown[:,0], xzdown[:,1]-toothBaseShrink,
+  ctc[:,None] + ballrad*np.vstack((np.cos(rho), np.sin(rho))),
+  xzdown[:,3]+toothBaseShrink, xzdown[:,4]))
+xzup[0,-3] = min(xzup[0,-3:])  #  since otherwise numerical error could spoil non-decreasingness
 
-xzup = np.vstack((irf*(1-np.array([th+2*ballrad,th,mitreMargin,th,mitreMargin,0,-2*ballrad])/ird),
- np.array([2*ballrad,0,-v,-th+mitreMargin+v,-th+mitreMargin,-th,-th-2*ballrad])))  # all nodes used on any path
-breakpoint()
-
-# xzmast = np.vstack((irf*(1-np.array([th+2*ballrad,th,th,mitreMargin,mitreMargin,0,-2*ballrad])/ird),
-#   np.array([2*ballrad,0,-v,-th+mitreMargin+v,-th+mitreMargin,-th,-th-2*ballrad])))  # all nodes used on any path
+numCornerCuts = int(np.ceil(edgeHalfLen/offset-(cutsPerTooth//2)*numTeeth))
+xz= [xzflat]*numCornerCuts+([xzup]*(cutsPerTooth//2)+[xzdown]*(cutsPerTooth//2))*numTeeth+[xzflat]*numCornerCuts
+xz = ([xzflat]*numCornerCuts
+  + ([xzflat]*((cutsPerTooth-2)//4) +[xzup] +[xzflat]*((cutsPerTooth-2)//4) +[xzdown]*(cutsPerTooth//2)) *numTeeth
+  + [xzflat]*numCornerCuts)
+maxAbsY = offset*0.5*(len(xz)-1)
+y = np.linspace(-maxAbsY, maxAbsY, len(xz))
+teethtooth = (cnc.PathGrid(y,xz)-originHeight).MultiToolGreedy(th-originHeight, gcodeToolSeq, yinc=True)[0]
 
 #### ##    CREATE THE TOOLPATH FOR THE EDGES WITHOUT TEETH:    ## #####
 
 # Actual vertices are [xzmast[0,{0,6}], {+,-}edgeHalfLen, xzmast[1,{0,6}].
 # We want the cuts in the long direction, though, so we'll construct this at 90deg & then rotate it back.
 # Thus transformed vertices are [{+,-}edgeHalfLen, xzmast[0,{0,6}], xzmast[1,{0,6}].
-s = np.linspace(0, 1, 1*int(np.ceil((xzmast[0,6]-xzmast[0,0])/offset)))  #  multiplying by 3 since these edges were a bit rough in the test cut (no; can just sand)
+s = np.linspace(0, 1, 1*int(np.ceil((xzflat[0,1]-xzflat[0,0])/offset)))
 
-flat = cnc.PathGrid(xzmast[0,0]+(xzmast[0,6]-xzmast[0,0])*s,
-  [np.array([[-edgeHalfLen,edgeHalfLen],2*[xzmast[1,0]+(xzmast[1,6]-xzmast[1,0])*sval]]) for sval in s])   #   HIPPO
-# cutpath = flat.castToMold(ballrad, ctol, np.NINF)
+flat = cnc.PathGrid(xzflat[0,0]+(xzflat[0,1]-xzflat[0,0])*s,
+  [np.array([[-edgeHalfLen,edgeHalfLen],2*[xzflat[1,0]+(xzflat[1,1]-xzflat[1,0])*sval]]) for sval in s])
 flattooth = (flat+th-originHeight).MultiToolGreedy(th-originHeight, gcodeToolSeq, yinc=True)[0]
 flattooth = flattooth.afxform(np.array([[0,1,0,0],[-1,0,0,0],[0,0,1,0],[0,0,0,1]]))
 
 
+##################################################################################################################
 #### ##    NOW COMBINE FIVE EDGES TO CREATE THE PIECE:    ## #####
+##################################################################################################################
 
 theta = 0.4*np.pi
 c = np.cos(theta)
@@ -179,8 +205,8 @@ s = np.sin(theta)
 R = np.array([[c,-s,0,0],[s,c,0,0],[0,0,1,0],[0,0,0,1]])
 
 for numJointEdges in [2,3,4,5]:
-  alltooth = cnc.catToolPaths(list(teethtooth.afxform(np.linalg.matrix_power(R,e)) for e in range(numJointEdges)) +
-    list(flattooth.afxform(np.linalg.matrix_power(R,e)) for e in range(numJointEdges,5)))
+  alltooth = cnc.catToolPaths(list(teethtooth.afxform(np.linalg.matrix_power(R,e)) for e in range(numJointEdges))
+    + list(flattooth.afxform(np.linalg.matrix_power(R,e)) for e in range(numJointEdges,5)))
   if boolPlot:
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d') 
@@ -196,13 +222,14 @@ for numJointEdges in [2,3,4,5]:
 
 # Actually rotate these into position to look at the geometry of the equatorial clamping jig.
 
-######################################################################################################################################
-#####     APPENDIX A:  VERTICES' COORDINATES     #####################################################################################
-######################################################################################################################################
+##################################################################################################################
+#####     APPENDIX A:  VERTICES' COORDINATES     #################################################################
+##################################################################################################################
 """
-This script creates gcode files for cutting the faces of a dodecahedral box. The size parameter we're using, rf, is defined as the
-planar exradius of the exterior faces, i.e., the distance from the centre of a face to a vertex of that face. To obtain coordinates of
-the exterior vertices, multiply the following vectors by rf/2, and note that phi denotes the "golden ratio" (1+sqrt(5))/2:
+This script creates gcode files for cutting the faces of a dodecahedral box. The size parameter we're using, rf,
+is defined as the planar exradius of the exterior faces, i.e., the distance from the centre of a face to a vertex
+of that face. To obtain coordinates of the exterior vertices, multiply the following vectors by rf/2, and note
+that phi denotes the "golden ratio" (1+sqrt(5))/2:
 
 BOTTOM FACE:
           phi                 -phi+1              -2                  -phi+1              phi
