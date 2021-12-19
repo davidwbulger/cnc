b426d8a2b8886b14a228776b9bfecfce6d5f2a53
unknown <davidwbulger@gmail.com>
2021-12-19 11:43:49 +1100

    Adding passingRelief.py, to make multipass relief cuts.

---
+++ passingRelief.py
@@ -0,0 +1,55 @@
+# Load an stl file, transform it into relief and save gcode to cut multiple
+# passes.
+
+import cnc
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats
+import sys
+
+if len(sys.argv) != 9:
+  print("Usage: python relief.py modName height depth bkgRad stckHt bitRad" +
+    " offset numPasses")
+  print("where")
+  print(" modName    is the name, sans extension, of stl model;")
+  print(" height     is the required horizontal extent of the carving;")
+  print(" depth      is the carving's vertical depth (flat bkgd to proudest);")
+  print(" bkgRad     is the radius of the flat circular background;")
+  print(" stckHt     is the height of the orig stock wrt the bit's home pos;")
+  print(" bitRad     is the radius of the ball-nosed butting bit;")
+  print(" offset     is the lateral offset between toolpaths;")
+  print(" numPasses  is the number of passes to do.")
+  print("All measurements are in mm.")
+else:
+  modName = sys.argv[1]
+  (height, depth, bkgRad, stckHt, bitRad, offset) = tuple(
+    float(a) for a in sys.argv[2:8])
+  numPasses = int(sys.argv[8])
+  if stckHt >= 0:
+    raise ValueError("The stckHt parameter should be negative.")
+  pt = cnc.polyTri(f"./{modName}.stl")
+  print(pt)
+
+  # Assume raw model height & depth are both 1. Scale as required:
+  pt = pt.afxform(np.array(
+    [[height,0,0,0],[0,height,0,0],[0,0,depth,0],[0,0,0,1]]))
+
+  # Also add a floor at (0,0,-depth) of radius bkgRad:
+  sector = 2*np.pi/30
+  for n in range(30):
+    pt.addConvexPolygon(np.array([
+      [0,0,-depth],
+      [bkgRad*np.cos(n*sector),bkgRad*np.sin(n*sector),-depth],
+      [bkgRad*np.cos((n+1)*sector),bkgRad*np.sin((n+1)*sector),-depth]]))
+
+  print("Final shape:")
+  print(pt)
+
+  pg = pt.toPG(offset)
+
+  # WRITE THE GCODE:
+  # tp = pg.SingleToolNoOpt(bitRad)
+  cude = depth/(numPasses-0.3)  #  make final pass a little shallower
+  tp = pg.MultiToolGreedy(stckHt,[{'bitrad':bitRad,'cude':cude,'ds':1}],
+    ymono=True)
+  tp.PathToGCode(1000, f"{modName}.gcode")
