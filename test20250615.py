adad779d0c41198007cda4f7df3e711998735693
unknown <davidwbulger@gmail.com>
2025-06-15 13:48:25 +1000

    A new simple test file.

---
+++ test20250615.py
@@ -0,0 +1,12 @@
+# Just a simple test. Goals are:
+  # start to remind myself how to use my cnc.py library
+  # test set-up after
+    # a two-year hiatus
+    # migration to gSender
+# This just cuts a square, in two passes. My intention is just to "cut air."
+
+import cnc
+import numpy as np
+
+(x,y) = np.array([[0, 0], [0,40], [40, 40], [40, 0], [0,0]]).T
+cnc.cutPath(x, y, 3, 2, 300, 3, "testSquare.gcode")
