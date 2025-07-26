0caeb955ffd280a13219625715296456ad2623a9
David Bulger <davidwbulger@gmail.com>
2025-07-26 15:17:14 +1000

    Debugging the groove cutter

--- GrooveForHenry.py
+++ GrooveForHenry.py
@@ -17,10 +17,11 @@ longStraight = 435  #  a little over half the length of the long straight
 shortStraight = 309.8
 cornerRadius = 85
 grooveWidth = 10
-bitWidth = 6  #  not really sure about this
+bitWidth = 3.175  #  not really sure about this
+numParallelCuts = 4
 
 # CALCULATED:
-offsets = np.array([-1,1]) * (grooveWidth - bitWidth) / 2
+offsets = np.linspace(-1,1,numParallelCuts) * (grooveWidth - bitWidth) / 2
 halfRectWidth = shortStraight / 2 + cornerRadius
 
 ##  CUT PATH  #################################################################
@@ -36,7 +37,8 @@ cuts = [
     [ [halfRectWidth+offset, shortStraight+cornerRadius] ] )
   for offset in offsets]
 
-(x,y) = np.vstack((cuts[0], cuts[1][::-1])).T
+# (x,y) = np.vstack((cuts[0], cuts[1][::-1])).T
+(x,y) = np.vstack([cuts[k][::((-1)**k)] for (k,cut) in enumerate(cuts)]).T
 
 cnc.cutPath(x, y, 35, 4, 300, -25, "Henry.gcode", 1.3)
 # START FROM 30mm ABOVE SURFACE!!!
