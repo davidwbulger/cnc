436acfbef9fb8c5f8cee9edf97b26686046d9647
David Bulger <davidwbulger@gmail.com>
2025-07-27 13:19:35 +1000

    Final tweaking

--- GrooveForHenry.py
+++ GrooveForHenry.py
@@ -25,21 +25,26 @@ offsets = np.linspace(-1,1,numParallelCuts) * (grooveWidth - bitWidth) / 2
 halfRectWidth = shortStraight / 2 + cornerRadius
 
 ##  CUT PATH  #################################################################
+# longStraight = 2*longStraight - shortStraight  #  fix earlier blunder
+
 cuts = [
   np.array(
-    [ [-halfRectWidth-offset, shortStraight+cornerRadius] ] +
+    [ [-halfRectWidth-offset, longStraight+cornerRadius] ] +
     [ [-shortStraight/2 + (cornerRadius+offset) * np.cos(phi),
       cornerRadius + (cornerRadius+offset) * np.sin(phi)]
       for phi in np.linspace(np.pi, 1.5*np.pi, 91)] + 
     [ [shortStraight/2 + (cornerRadius+offset*0.8) * np.cos(phi),
       cornerRadius + (cornerRadius+offset) * np.sin(phi)]
       for phi in np.linspace(1.5*np.pi, 2*np.pi, 91)] + 
-    [ [halfRectWidth+offset*0.8, shortStraight+cornerRadius] ] )
+    [ [halfRectWidth+offset*0.8, longStraight+cornerRadius] ] )
   for offset in offsets]
 
 # (x,y) = np.vstack((cuts[0], cuts[1][::-1])).T
 (x,y) = np.vstack([cuts[k][::((-1)**k)] for (k,cut) in enumerate(cuts)]).T
 
-cnc.cutPath(x, y, 35, 4, 500, -25, "Henry.gcode", 1.5)
 # cnc.cutPath(x, y, 31, 1, 500, -25, "Henry.gcode")
+
+cnc.cutPath(x, y, 35, 4, 750, -25, "Henry.gcode", 1.5)
+cnc.cutPath(-x, y, 35, 4, 750, -25, "HenryWiderAtR.gcode", 1.5)
+
 # START FROM 30mm ABOVE SURFACE!!!
